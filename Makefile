b4cc79e875f6e71fa9bf2156c4050fbf13fc861a
Calvin Metcalf <cmetcalf@appgeo.com>
2013-11-06 13:16:59 -0500

    add third

--- Makefile
+++ Makefile
@@ -1,4 +1,6 @@
 first:
-	browserify -e examples/makeMap.js -o bundle.js
+	browserify -e examples/makeMap.js -o leaflet-bundle.js
 second:
-	browserify -e examples/addgeojson.js -o bundle.js
+	browserify -e examples/addgeojson.js -o leaflet-bundle.js
+third:
+	browserify -e examples/makeHashMap.js -o leaflet-bundle.js
--- examples/makeHashMap.js
+++ examples/makeHashMap.js
@@ -1,6 +1,10 @@
 var L = require('leaflet');
+L = require('./leaflet.hash')(L);
 var map = L.map('map');
-map.setView([ 42.32250876543571, -71.01837158203125], 10);
+if(!location.hash){
+	map.setView([ 42.32250876543571, -71.01837158203125], 10);
+}
+map.addHash();
 var stamenToner = require('./stamenToner');
 stamenToner.addTo(map);
 module.exports = map;
